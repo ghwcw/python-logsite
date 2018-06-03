81fa3496e4349fb3bbd619168ea7b6e4934fc3c2
wangchunwang <wcwnina@163.com>
2018-06-03 18:27:36 +0800

    增加了ModelAdmin类

--- Project/logsite/login/admin.py
+++ Project/logsite/login/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from login import models
+from ..login import models
+
+
 # Register your models here.
 
+class UserAdmin(admin.ModelAdmin):
+    empty_value_display = '-NULL-'
 
-admin.site.register(models.User)
-admin.site.register(models.ConfirmString)
+
+class ConfirmStringAdmin(admin.ModelAdmin):
+    empty_value_display = '-NULL-'
 
 
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.ConfirmString, ConfirmStringAdmin)
